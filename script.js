d884e8ac89adb130a9875d52eb5d119ec8484368
Artem Kuskin <kuskin.fortech@gmail.com>
2022-08-01 17:48:05 +0300

    fix bugs

--- script.js
+++ script.js
@@ -1,22 +1,16 @@
-
 let allTasks = JSON.parse(localStorage.getItem('tasks')) || []; 
-let newArray = allTasks.slice();
 let activeEditTask = null;
-let currentText = "";
-const url = 'http://localhost:8000'
+let currentText = '';
+const url = 'http://localhost:8000';
 const fetchHeaders = {
   'Content-Type': 'application/json;charset=utf-8',
   'Access-Control-Allow-Origin': '*'
 }
 
-
 window.onload = async  () => {
   try {
     const input = document.getElementById("add-task");
-    input.addEventListener('change', updateValue);
-    const resp = await fetch(`${url}/tasks`, {
-      method: 'GET'
-    });
+    const resp = await fetch(`${url}/tasks`);
     let result = await resp.json();
     console.log(result);
     render();
@@ -35,7 +29,7 @@ const taskAdd = async () => {
         text: input.value,
       })
     }); 
-    const result = await resp.json()
+    const result = await resp.json();
     allTasks.push(result);
     localStorage.setItem('tasks', JSON.stringify(allTasks));
     input.value = '';
@@ -45,55 +39,49 @@ const taskAdd = async () => {
   } 
 }
 
-const updateValue = async (event) => { 
-  const input = document.getElementById("add-task"); 
-  input.value = event.target.value;
-}
-
 const render = () => { 
   const content = document.getElementById('content-page'); 
   while(content.firstChild) { 
     content.removeChild(content.firstChild);
   }
-  newArray.sort((a, b) => a.isCheck > b.isCheck ? 1 : a.isCheck < b.isCheck ? -1 : 0) 
-  allTasks = newArray;
+  allTasks.sort((a, b) => a.isCheck > b.isCheck ? 1 : a.isCheck < b.isCheck ? -1 : 0);
   allTasks.forEach((item, index) => { 
     const{text, isCheck, _id} = item;
-    const buttonDoneTask = document.createElement('button')
+    const buttonDoneTask = document.createElement('button');
     const imageDone = document.createElement('img');
     imageDone.src = 'img/done.svg';
-    imageDone.id = `task_button_done${_id}`
-    imageDone.alt = "";
+    buttonDoneTask.id = `task_button_done${_id}`;
+    imageDone.alt = '';
     buttonDoneTask.appendChild(imageDone);
     buttonDoneTask.onclick =  () => {
-      doneEditTask();
+      updateTaskText(_id);
     };
     
     const cancel = document.createElement('img');
-    const buttonCancelEdit = document.createElement('button')
+    const buttonCancelEdit = document.createElement('button');
     cancel.src = 'img/cancel.svg';
-    cancel.id = `task_cancel${_id}`;
-    cancel.alt = ""
+    buttonCancelEdit.id = `task_cancel${_id}`;
+    cancel.alt = '';
     buttonCancelEdit.appendChild(cancel);
     buttonCancelEdit.onclick =  () => {
       cancelEdit();
     };
     
     const imageEdit = document.createElement('img'); 
-    const buttonEditTask = document.createElement('button')
+    const buttonEditTask = document.createElement('button');
     imageEdit.src = 'img/edit.svg'; 
-    imageEdit.id = `task_button_edit${_id}`;
-    imageEdit.alt = "";
+    buttonEditTask.id = `task_button_edit${_id}`;
+    imageEdit.alt = '';
     buttonEditTask.appendChild(imageEdit);
-    buttonEditTask.onclick =  () => { 
+    buttonEditTask.onclick =   () => { 
       editTask(_id);
     };
 
     const imageDelete = document.createElement('img'); 
-    const buttonDeleteTask = document.createElement('button')
+    const buttonDeleteTask = document.createElement('button');
     imageDelete.src = 'img/delete.svg';
-    imageDelete.id = `task_button_delete${_id}`
-    imageDelete.alt = "";
+    buttonDeleteTask.id = `task_button_delete${_id}`
+    imageDelete.alt = '';
     buttonDeleteTask.appendChild(imageDelete);
     buttonDeleteTask.onclick =  () => { 
       onDeleteTask(_id);
@@ -115,18 +103,19 @@ const render = () => {
       const inputTask = document.createElement('input');
       inputTask.type = 'text';
       inputTask.value = text;
-      inputTask.addEventListener('change', updateTaskText);
+      // inputTask.addEventListener('change', updateTaskText);
+      //inputTask.addEventListener('change', () => updateTaskText(_id))
       container.appendChild(inputTask);
       checkbox.remove();
       container.appendChild(buttonDoneTask);
       container.appendChild(buttonCancelEdit); 
       } else {
-      const text2 = document.createElement('p');  
-      text2.innerText = text;  
-      text2.className = isCheck ? 'text-task done-text' : 'text-task'; 
-      container.appendChild(text2); 
+      const newText = document.createElement('p');  
+      newText.innerText = text;  
+      newText.className = isCheck ? 'text-task done-text' : 'text-task'; 
+      container.appendChild(newText); 
       container.appendChild(buttonEditTask);  
-      container.appendChild(buttonDeleteTask) 
+      container.appendChild(buttonDeleteTask);
       }
       if(allTasks[index].isCheck) {
         buttonEditTask.remove();
@@ -158,33 +147,54 @@ const onChangeCheckbox = async (_id, isCheck) => {
 }
 
 const cancelEdit = () => {
-  allTasks[activeEditTask].text = currentText;
   activeEditTask = null;
   localStorage.setItem('tasks', JSON.stringify(allTasks));
-  render()
+  render();
 }
 
 const onDeleteTask = async (_id) => { 
   try {
-  const resp =  await fetch(`${url}/tasks/${_id} `, {
+    const resp =  await fetch(`${url}/tasks/${_id} `, {
       method: 'DELETE',
       headers: fetchHeaders
     });
-    const result = await resp.json()
+    const result = await resp.json();
     if (result.deletedCount > 0) {
       allTasks = allTasks.filter(item => (item._id !== _id));
     }
     localStorage.setItem('tasks', JSON.stringify(allTasks));
     render();
   } catch {
-    console.error('Failed delete task')
+    console.error('Failed delete task');
   }
 }
 
-const updateTaskText = (event) => { 
-  allTasks[activeEditTask].text = event.target.value;
-  localStorage.setItem('tasks', JSON.stringify(allTasks));
-  render();
+const updateTaskText = async (_id) => { 
+  try {
+    const input = document.querySelector(`#task-${_id} input`);
+    if (!input || !input.value) {
+      return;
+    }
+    if (input.value.trim() === '') {
+      console.error('Поле пустое');
+    }
+    const resp = await fetch(`${url}/tasks/${_id}/text`, {
+      method: 'PATCH',
+      headers: fetchHeaders,
+      body: JSON.stringify({
+        text: input.value
+      })
+    });
+    const result = await resp.json();
+    allTasks.forEach(item => {
+      if (item._id === result._id) {
+        item.text = result.text;
+      }
+    });
+    render();
+    } catch {
+      console.error('Fail to change');
+    } 
 }
 
 const doneEditTask = () => { //Функция сохранения
@@ -198,7 +208,7 @@ const allDelete = async () => {
     const resp =  await fetch(`${url}/tasks`, {
       method: 'DELETE',
     })
-    allTasks.splice(0);
+    allTasks = [];
     localStorage.setItem('tasks', JSON.stringify(allTasks));
     render();
   } catch {
@@ -206,28 +216,8 @@ const allDelete = async () => {
   }
 }
 
-const editTask = async (_id) => {
-  try {
-    const input = document.getElementById(`task_button_edit${_id}`)
-    if (input.value === null) {
-      return;
-    }
-    const resp = await fetch(`${ url }/tasks/${ _id }/text`, {
-      method: 'PATCH',
-      headers: fetchHeaders,
-      body: JSON.stringify({
-        text: input.value
-      })
-    });
-    const result = await resp.json();
-    allTasks.forEach(item => {
-      if (item._id === result._id) {
-        item.text = result.text;
-      }
-    });
-    currentText = allTasks[activeEditTask].text;
+  const editTask = (_id, text) => {
+    activeEditTask = _id;
+   currentText = text;
     render();
-  } catch {
-    console.error('Fail to change');
   }
-}
